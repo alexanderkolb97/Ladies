a6ceae53ef5cf12333459029bb1d258a5ef936e0
Sasha <kolb10@mail.ru>
2022-09-27 20:12:06 +0300

    massive code refactoring

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,8 @@
 import {Context} from './App';
 import {useContext} from 'react';
 import {Link} from "react-router-dom";
+import Logo from './images/logo.png'
+import Cart from './images/cart.png'
 
 function Header() {
     const {setSearchstatus, cartNumber} = useContext(Context);
@@ -9,7 +11,7 @@ function Header() {
       if (e.target.value.length < 3) {
         setSearchstatus('');
 
-        return;
+        return null;
       }
 
       setSearchstatus(e.target.value);
@@ -18,15 +20,15 @@ function Header() {
     return (
         <header className="header">
             <div className="logo">
-                <a href="/"><img src="./logo.png" alt="logo"/></a>
+                <a href="/"><img src={Logo} alt="logo"/></a>
             </div>
             <div className="search_wrapper">
-                <input onChange={searchProducts} type="text" name="search" placeholder="Search"/>
+                <input onChange={searchProducts} type="text" name="search" placeholder="Search" />
                 <span className="search_icon"></span>
             </div>
             <Link to="/cart" className="cart">
                 <span className="cart_amount">{cartNumber}</span>
-                <img src="cart.png" alt="cart"/>
+                <img src={Cart} alt="cart"/>
             </Link>
         </header>
     )
